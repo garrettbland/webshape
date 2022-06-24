072f225683a4f985f025286599cb7d8adc7dea99
Garrett Bland <gmorganbland@gmail.com>
2022-06-23 21:03:44 -0500

    feat: setup express

---
+++ src/template.ts
@@ -0,0 +1,112 @@
+import { parse, render, getConfig, filters } from 'squirrelly'
+import { uniqBy } from 'lodash-es'
+import { TemplateObject } from 'squirrelly/dist/types/parse.js'
+
+/**
+ * Example HTML
+ */
+const EXAMPLE_HTML =
+    "<h1>{{ it.title | text }}</h1><p>{{ it.title | text }}</><img src='{{ it.image | image }}'><p>{{ it.description | content }}</p>"
+
+/**
+ * Example DB
+ */
+const DB_RECORD = {
+    name: 'garrett',
+    hero_image: 'https://example.com/image.jpg',
+    title: 'Example site',
+}
+
+/**
+ * Merge custom config options into Squirelly default config
+ */
+const CONFIG = getConfig({
+    tags: ['{{', '}}'],
+})
+
+/**
+ * Custom filters. Used for our purpose as "tags" to define type of
+ * content in template - text, image, content, etc. Reason being that
+ * the filters are shown in Ast object from parsing
+ */
+const FILTERS = ['text', 'content', 'image']
+FILTERS.forEach((filterItem) => filters.define(filterItem, (originalVal) => originalVal))
+
+/**
+ * Parses HTML template, grabs Ast objects, removes duplicates, and
+ * then returns array of normalized readable objects
+ */
+const getDynamicItems = (HTML: string) => {
+    /**
+     * Parse HTML string into AST Tree
+     */
+    const parsedHTML = parse(HTML, CONFIG)
+
+    /**
+     * Filter out everything except for AST Objects. These objects
+     */
+    const allTreeObjects = parsedHTML.filter((i) => i === Object(i))
+
+    /**
+     * Remove duplicates. Example - Page title might be used in multiple
+     * places.
+     */
+    const treeObjects = uniqBy(allTreeObjects, 'c')
+
+    /**
+     * Normalize data into something we can use and better understand
+     * in the rest of our application. Example shape returned by default.
+     * Returns array of objects and slices of "it." from "c" value
+     * ```
+     * { f: [ [Array] ], c: 'it.title', t: 'i'}
+     * ```
+     */
+    const normalizedTreeItems = (treeObjects as TemplateObject[]).map((item) => {
+        return {
+            key: item?.c?.slice(3),
+        }
+    })
+
+    return normalizedTreeItems
+}
+
+const build = () => {
+    console.log('Building webpage...')
+
+    /**
+     * Parse HTML and get data needed for template
+     */
+    const options = getDynamicItems(EXAMPLE_HTML)
+    console.log(`This template needs ${options.map(({ key }) => key).join(', ')}`)
+    console.log(`Select stuff from db`)
+
+    /**
+     * Create object to be injected into template
+     */
+    const requiredTemplateObject = options.reduce((previousValue, nextValue) => {
+        return {
+            ...previousValue,
+            [nextValue.key as string]: 'Default...',
+        }
+    }, {})
+
+    /**
+     * Merge required object and default values with values from database
+     */
+    const templateData = {
+        ...requiredTemplateObject,
+        ...DB_RECORD,
+    }
+
+    /**
+     * Render html with template data
+     */
+    const rendered = render(EXAMPLE_HTML, templateData)
+
+    /**
+     * Send to client
+     */
+    console.log(rendered)
+}
+
+build()
