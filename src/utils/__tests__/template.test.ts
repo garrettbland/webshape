a038c3a14c9215a32d0db2d599a44bdc95b35d06
Garrett Bland <gmorganbland@gmail.com>
2022-07-07 00:39:07 -0500

    feat: template dynamic items test

--- src/utils/__tests__/template.test.ts
+++ src/utils/__tests__/template.test.ts
@@ -1,8 +1,41 @@
+import { getDynamicItems, getFilter } from '../template'
+
 describe('template', () => {
     describe('getDynamicItems', () => {
-        it.todo('Gets dynamic items from HTML string')
+        it('Gets dynamic items from HTML string', () => {
+            const testHTML = '<p>{{ it.title | text | label("Title") }}'
+            const testAST = getDynamicItems(testHTML)
+            expect(testAST[0].key).toBe('title')
+            expect(testAST[0].type).toBe('text')
+        })
+        it('Should only return single key/value for duplicates', () => {
+            const testHTML = `
+                <p>{{ it.title | text }}</p>
+                <img src="{{ it.heroImage | image }}" alt="{{ it.title | text}}">
+            `
+            const testAST = getDynamicItems(testHTML)
+            expect(testAST.length).toBe(2)
+            expect(testAST[0].key).toBe('title')
+            expect(testAST[0].type).toBe('text')
+            expect(testAST[1].key).toBe('heroImage')
+            expect(testAST[1].type).toBe('image')
+        })
     })
     describe('getFilter', () => {
-        it.todo('Should find first available filter from template tag')
+        it('Should find first available filter from template tag', () => {
+            const filters = [['text', '']] as any
+            expect(getFilter(filters)).toBe('text')
+        })
+        it('Should find first available filter from template tag & label', () => {
+            const filters = [
+                ['image', ''],
+                ['label', 'Background Image'],
+            ] as any
+            expect(getFilter(filters)).toBe('image')
+        })
+        it('Should return null if no filter is added', () => {
+            const filters = [] as any
+            expect(getFilter(filters)).toBe(null)
+        })
     })
 })
