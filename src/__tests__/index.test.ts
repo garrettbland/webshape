a8e20fc8cb624e424333a80181fd6e57c57b7048
Garrett Bland <gmorganbland@gmail.com>
2022-07-14 23:13:07 -0500

    feat: index test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,20 @@
-/**
- * Example test
- */
+import * as fastifyApp from '../app'
+import * as developmentModules from '../utils/development'
 
-describe('Example', () => {
-    it('Should return 1', () => {
-        expect(1).toBe(1)
+describe('Server', () => {
+    it('Should start the server on a port 3000', () => {
+        jest.spyOn(developmentModules, 'getPort').mockReturnValueOnce(3000)
+        const listenMock = jest.fn()
+        jest.spyOn(fastifyApp, 'getApp').mockImplementationOnce(
+            () =>
+                ({
+                    listen: listenMock,
+                } as any)
+        )
+
+        require('../index')
+
+        expect(listenMock).toBeCalledTimes(1)
+        expect(listenMock).toBeCalledWith({ port: 3000 })
     })
 })
