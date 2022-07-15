f957ac82a2b75bb9e3665c5e6b4ec5e749e49df1
Garrett Bland <gmorganbland@gmail.com>
2022-07-14 22:10:39 -0500

    feat: notFound test

--- src/controllers/notFound.ts
+++ src/controllers/notFound.ts
@@ -4,5 +4,5 @@ import { FastifyReply } from 'fastify'
  * 404 Not Found Controller
  */
 export const notFound = (res: FastifyReply) => {
-    res.type('text/html').status(404).send('<h1>404 not found</h1>')
+    res.type('text/html; charset=utf-8').status(404).send('<h1>404 not found</h1>')
 }
